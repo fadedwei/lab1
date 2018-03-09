8f3a2369b13f0904df0c51610a8d74e01ead14f3
fadedwei <wei831127@hotmail,com>
2018-03-09 17:34:57 +0800

    I finish lab1.

---
+++ lab1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int number;
+    cout << "Please enter a number" << endl;
+    cin >> number;
+    cout << number << "  ";
+    while(number != 1){
+        if(number % 2 == 1 ){
+    	    number = 3*number + 1;
+        }else{
+    	    number = number / 2;
+        }
+	cout << number <<"  ";
+    }
+    return 0;
+
+}
